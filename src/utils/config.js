36938cf6cf8c106f6fbb27e207c6ad161cd935e9
Roman Nikolaenkov <rnikolaenkov@gmail.com>
2021-07-01 02:20:28 +0300

    поправил конфиг для сервера

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 export const apiConfig = {
   externalUrl: 'https://api.nomoreparties.co',
   externalApi: 'https://api.nomoreparties.co/beatfilm-movies',
-  // api: 'https://api.rnikolaenkov.nomoredomains.club',
-  api: 'http://localhost:3001',
+  api: 'https://api.rnikolaenkov.nomoredomains.club',
+  // api: 'http://localhost:3001',
 }
